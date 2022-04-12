e991f0a0ec15375c07cfb241e9bdbc31d0a5d8b7
BOCHATAY Yannick <yannick.bochatay@meteo.fr>
2022-04-12 17:33:52 +0200

    add try catch block

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const puppeteer = require("puppeteer")
 
 ;(async() => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+    try {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
 
-  await page.emulateMediaType("screen")
-  await page.goto("https://yannickbochatay.github.io/puppeteer-pdf-tests/examples/example1.html", { waitUntil : "load" })
-  await page.pdf({ printBackground : true })
+        await page.emulateMediaType("screen")
+        await page.goto("https://yannickbochatay.github.io/puppeteer-pdf-tests/examples/example1.html", { waitUntil : "load" })
+        await page.pdf({ printBackground : true })
 
-  process.exit()
+    } catch (e) {
+        console.log(e)
+    }
+
+    process.exit()
 })()
